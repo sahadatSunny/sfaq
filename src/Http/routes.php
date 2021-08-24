6126b09a9be020a562c37505003ea3fe83b9e543
sahadat <sahadat.msun@gamil.com>
2021-08-24 16:24:14 +0600

    initializing git

---
+++ src/Http/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('sfaq', function(){
+    echo "hello from sFAQ";
+});
---
+++ src/SfaqServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Sahadat\Sfaq;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class SfaqServiceProvider extends ServiceProvider
+{
+ 
+    public function boot(){
+
+        $this->registerRoutes();
+
+    }
+
+
+
+    public function register(){
+
+
+
+    }
+
+
+    private function registerRoutes()
+    {
+        Route::group($this->routeConfiguration(), function () {
+            $this->loadRoutesFrom(__DIR__.'/Http/routes.php');
+        });
+    }
+    
+
+}
